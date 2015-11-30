5e34d7e9be9fbf8b123698f266156a74496fe53e
Wu Young <doomsplayer@gmail.com>
2015-11-30 15:27:19 +0800

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -325,10 +325,8 @@ impl<'a> ToFilterValue for &'a String {
 
 impl<I: ToFilterValue> ToFilterValue for Vec<I> {
     fn to_filter(&self) -> String {
-        self.iter().fold(String::new(), |mut acc, elem| {
-            acc.push_str(&elem.to_filter());;
-            acc
-        })
+        let s = self.iter().map(|i| i.to_filter()).join(",");
+        format!("[{}]", s)
     }
 }
 /// Type defines a metric to query.
