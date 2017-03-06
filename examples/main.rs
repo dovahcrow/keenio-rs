df96228aa8b9f7f0a820e7ff1aa82ec610847541
Young Wu <doomsplayer@gmail.com>
2017-03-06 19:09:21 +0800

    update dependencies

--- examples/main.rs
+++ examples/main.rs
@@ -1,30 +1,22 @@
 extern crate keen;
 extern crate chrono;
-extern crate rustc_serialize;
-extern crate docopt;
-
+#[macro_use]
+extern crate structopt_derive;
+extern crate structopt;
 use keen::{KeenClient, Filter, Metric, TimeFrame};
 use std::io::Read;
 use std::{time, env};
 use chrono::*;
+use structopt::StructOpt;
 
-
-static USAGE: &'static str = "
-Usage:
-  main <from> <to>
-";
-
-#[derive(Debug, RustcDecodable)]
+#[derive(StructOpt, Debug, Clone)]
 struct Args {
-    arg_from: i64,
-    arg_to: i64,
+  from: i64,
+  to: i64,
 }
 
-
 fn main() {
-    let args: Args = docopt::Docopt::new(USAGE)
-        .and_then(|d| d.decode())
-        .unwrap_or_else(|e| e.exit());
+    let args = Args::from_args();
 
     let key = env::var("KEEN_READ_KEY").unwrap();
     let proj = env::var("KEEN_PROJECT_ID").unwrap();
@@ -38,8 +30,8 @@ fn main() {
                              "strikingly_pageviews".into(),
                              TimeFrame::Absolute(UTC::now() - Duration::hours(10),
                                                  UTC::now() - Duration::hours(1)));
-    q.filter(Filter::gt("pageId", args.arg_from));
-    q.filter(Filter::lt("pageId", args.arg_to));
+    q.filter(Filter::gt("pageId", args.from));
+    q.filter(Filter::lt("pageId", args.to));
     q.group_by("normalized_referrer");
     q.group_by("ip_geo_info.country");
     q.group_by("parsed_user_agent.os.family");
